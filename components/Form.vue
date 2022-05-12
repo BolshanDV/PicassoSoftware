c64bba0e11b92c644d9ced38f572292b527a3a74
BolshanDV <92440782+BolshanDV@users.noreply.github.com>
2022-05-12 19:26:27 +0300

    final

---
+++ components/Form.vue
@@ -0,0 +1,146 @@
+<template>
+  <section class="company">
+    <section class="company__column">
+      <section class="container">
+        <section class="company_title">
+          <p>Компания</p>
+        </section>
+      </section>
+      <section>
+        <input
+            type="text"
+            placeholder="Введите название компании"
+            v-model="company"
+            class="text-field__input"
+            :class="{input_active: autocompleteObj.length}"
+
+        >
+        <section class="catalog"
+                 v-if="autocompleteObj.length"
+        >
+          <div
+              v-for="(companyItem, index) in autocompleteObj"
+              class="card"
+              @click="chooseCard(companyItem)"
+          >
+            <section class="card__img">
+              <img :src="companyItem.logo" alt="" class="logo">
+            </section>
+            <section class="card__body">
+              <div class="card__name">{{companyItem.name}}</div>
+              <div class="card__domain">{{companyItem.domain}}</div>
+            </section>
+          </div>
+        </section>
+
+      </section>
+    </section>
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Form",
+  data() {
+    return {
+      company: "",
+      autocompleteObj: [],
+      selectedName: ""
+    }
+  },
+  watch: {
+    company: function () {
+      if (this.company === '') {
+        this.autocompleteObj.length = 0
+      }
+      this.getCompany()
+    }
+  },
+  methods: {
+    async getCompany() {
+      axios
+          .get(`https://autocomplete.clearbit.com/v1/companies/suggest?query=${this.company}`)
+          .then(response => {
+            this.autocompleteObj = response.data
+          })
+          .catch(error => {
+            console.log(error);
+          })
+    },
+    chooseCard(companyItem){
+      this.company = companyItem.name
+    }
+  }
+}
+</script>
+
+<style scoped>
+.company{
+  width: 30%;
+}
+.company__column{
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+.company_title{
+  font-family: 'Roboto',serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 20px;
+  color: #484848;
+}
+.container{
+  margin-bottom: 8px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+}
+.company_input{
+  height: 33px;
+}
+.card{
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 2% 1%;
+}
+.logo {
+  width: 54px;
+  height: 54px;
+}
+.card__img {
+  margin-right: 5%;
+}
+.card__name {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 16px;
+  color: #000000;
+}
+.card__domain {
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 14px;
+  color: #9F9F9F;
+  margin-top: 4px;
+}
+.catalog {
+  border-width: 0 1px 1px 1px;
+  border-color: #E5E5E5;
+  border-style: solid ;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 0 0 4px 4px;
+  padding: 2% 10%;
+}
+.input_active {
+  border: 1px solid #E5E5E5;
+  border-radius: 4px 4px 0 0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,27 @@
 <template>
-  <Tutorial/>
+  <section class="form">
+    <section class="from__column">
+      <Form/>
+    </section>
+  </section>
+
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
 }
 </script>
+
+<style scoped>
+  .form{
+    margin-top: 10vh;
+    /*width: 100vw;*/
+  }
+  .from__column{
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+  }
+</style>
